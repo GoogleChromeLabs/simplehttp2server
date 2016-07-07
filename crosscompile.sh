e1d30400c3e35b2888f4a2c6e750cb701c2e20fd
Surma <surma@surma.link>
2016-07-07 12:21:22 +0100

    Update crosscrompile script

--- crosscompile.sh
+++ crosscompile.sh
@@ -3,5 +3,9 @@
 for target in darwin:amd64 linux:amd64 linux:386 linux:arm windows:amd64; do
   echo "Compiling $target"
   export GOOS=$(echo $target | cut -d: -f1) GOARCH=$(echo $target | cut -d: -f2)
-  bash -c "go build -o $(basename $(echo $PWD))_${GOOS}_${GOARCH} ."
+  EXT=""
+  if [ "$GOOS" = "windows" ]; then
+    EXT=".exe"
+  fi
+  bash -c "go build -o $(basename $(echo $PWD))_${GOOS}_${GOARCH}${EXT} ."
 done
