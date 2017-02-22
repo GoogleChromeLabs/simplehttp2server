4ca64bd154d79df43c79024b2aa67cbc005111a4
Surma <surma@surma.link>
2017-02-22 18:20:09 +0000

    Update brew fromula

--- simplehttp2server.rb
+++ simplehttp2server.rb
@@ -1,9 +1,9 @@
 class Simplehttp2server < Formula
   desc "SimpleHTTP2Server"
   homepage "https://github.com/GoogleChrome/simplehttp2server"
-  url "https://github.com/GoogleChrome/simplehttp2server/releases/download/3.0.0/simplehttp2server_darwin_amd64"
-  sha256 "af017a8d927e620233f17275e9c86a5d2ce59fa8616306e4e6679bf00ca566f7"
-  version "3.0.0"
+  url "https://github.com/GoogleChrome/simplehttp2server/releases/download/3.0.1/simplehttp2server_darwin_amd64"
+  sha256 "3a090db3f9f52ec240329ff08ec9fe5e7b07b41c78d081eb8e46036360d4ce67"
+  version "3.0.1"
 
   def install
     system "chmod", "+x", "simplehttp2server_darwin_amd64"
