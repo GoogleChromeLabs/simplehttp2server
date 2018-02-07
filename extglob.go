1ad5ace312b62a993e84a2278283ed217dfe0f59
Nuno Cruces <ncruces@gmail.com>
2018-02-07 14:01:56 +0000

    Improved glob pattern matching.

--- extglob.go
+++ extglob.go
@@ -1,25 +1,179 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"regexp"
+	"regexp/syntax"
 	"strings"
 )
 
-var (
-	parensExpr   = regexp.MustCompile("([^\\\\])\\(([^)]+)\\)")
-	questionExpr = regexp.MustCompile("\\?([^(])")
-)
-
 func CompileExtGlob(extglob string) (*regexp.Regexp, error) {
-	tmp := extglob
-	tmp = strings.Replace(tmp, ".", "\\.", -1)
-	tmp = strings.Replace(tmp, "**", ".🐷", -1)
-	tmp = strings.Replace(tmp, "*", fmt.Sprintf("[^%c]*", os.PathSeparator), -1)
-	tmp = questionExpr.ReplaceAllString(tmp, fmt.Sprintf("[^%c]$1", os.PathSeparator))
-	tmp = parensExpr.ReplaceAllString(tmp, "($2)$1")
-	tmp = strings.Replace(tmp, ")@", ")", -1)
-	tmp = strings.Replace(tmp, ".🐷", ".*", -1)
-	return regexp.Compile("^" + tmp + "$")
+	ctx := globctx{glob: extglob}
+	ctx.compileGlobstarPrefix()
+
+	if err := ctx.compileExpression(); err != nil {
+		return nil, err
+	}
+
+	return regexp.Compile("^" + string(ctx.regexp) + "$")
+}
+
+type globctx struct {
+	glob       string
+	regexp     []byte
+	pos, depth int
+}
+
+func (c *globctx) compileExpression() error {
+	for c.pos < len(c.glob) {
+		switch curr := c.glob[c.pos]; curr {
+		case '\\':
+			if err := c.compileEscapeSequence(); err != nil {
+				return err
+			}
+		case '*':
+			if err := c.compileSubExpression("(?:", ")*", "[^/]*"); err != nil {
+				return err
+			}
+		case '?':
+			if err := c.compileSubExpression("(?:", ")?", "[^/]"); err != nil {
+				return err
+			}
+		case '+':
+			if err := c.compileSubExpression("(?:", ")+", "\\+"); err != nil {
+				return err
+			}
+		case '@':
+			if err := c.compileSubExpression("(?:", ")", "\\@"); err != nil {
+				return err
+			}
+		case '!':
+			if err := c.compileSubExpression("(?~", ")", "\\!"); err != nil {
+				return err
+			}
+		case ')':
+			if c.depth > 0 {
+				return nil
+			}
+			c.regexp = append(c.regexp, "\\)"...)
+			c.pos += 1
+
+		case '|':
+			if c.depth > 0 {
+				c.regexp = append(c.regexp, '|')
+				c.pos += 1
+			} else {
+				c.regexp = append(c.regexp, "\\|"...)
+				c.pos += 1
+			}
+		case '/':
+			if c.depth == 0 && (c.glob[c.pos:] == "/**" || strings.HasPrefix(c.glob[c.pos:], "/**/")) {
+				c.regexp = append(c.regexp, "(?:/[^/]*)*"...)
+				c.pos += 3
+			} else {
+				c.regexp = append(c.regexp, '/')
+				c.pos += 1
+			}
+		case '[':
+			if err := c.compileCharacterClass(); err != nil {
+				return err
+			}
+		case '.', '^', '$', '(', '{':
+			c.regexp = append(c.regexp, '\\', curr)
+			c.pos += 1
+		default:
+			c.regexp = append(c.regexp, curr)
+			c.pos += 1
+		}
+	}
+
+	if c.depth > 0 {
+		return &syntax.Error{Code: syntax.ErrMissingParen, Expr: c.glob}
+	}
+	return nil
+}
+
+func (c *globctx) compileSubExpression(prefix string, suffix string, noexpr string) error {
+	if strings.HasPrefix(c.glob[c.pos+1:], "(") {
+		c.regexp = append(c.regexp, prefix...)
+		c.depth += 1
+		c.pos += 2
+		if err := c.compileExpression(); err != nil {
+			return err
+		}
+		c.regexp = append(c.regexp, suffix...)
+		c.depth -= 1
+		c.pos += 1
+	} else {
+		c.regexp = append(c.regexp, noexpr...)
+		c.pos += 1
+	}
+	return nil
+}
+
+func (c *globctx) compileCharacterClass() error {
+	c.regexp = append(c.regexp, '[')
+	c.pos += 1
+
+	if c.pos < len(c.glob) {
+		switch curr := c.glob[c.pos]; curr {
+		case ']', '-':
+			c.regexp = append(c.regexp, curr)
+			c.pos += 1
+
+		case '!', '^':
+			c.regexp = append(c.regexp, '^')
+			c.pos += 1
+
+			if strings.HasPrefix(c.glob[c.pos:], "]") {
+				c.regexp = append(c.regexp, ']')
+				c.pos += 1
+			}
+		}
+	}
+
+	for c.pos < len(c.glob) {
+		if s := c.glob[c.pos:]; strings.HasPrefix(s, "[:") {
+			if i := strings.Index(s[2:], ":]"); i >= 0 {
+				c.regexp = append(c.regexp, s[:4+i]...)
+				c.pos += 4 + i
+				continue
+			}
+		}
+
+		switch curr := c.glob[c.pos]; curr {
+		case '\\':
+			if err := c.compileEscapeSequence(); err != nil {
+				return err
+			}
+		case ']':
+			c.regexp = append(c.regexp, ']')
+			c.pos += 1
+			return nil
+		default:
+			c.regexp = append(c.regexp, curr)
+			c.pos += 1
+		}
+	}
+
+	return &syntax.Error{Code: syntax.ErrMissingBracket, Expr: c.glob}
+}
+
+func (c *globctx) compileEscapeSequence() error {
+	if c.pos+1 == len(c.glob) {
+		return &syntax.Error{Code: syntax.ErrTrailingBackslash, Expr: c.glob}
+	}
+	c.regexp = append(c.regexp, '\\', c.glob[c.pos+1])
+	c.pos += 2
+	return nil
+}
+
+func (c *globctx) compileGlobstarPrefix() {
+	if c.glob == "**" {
+		c.regexp = append(c.regexp, "(?:[^/]+(?:/[^/]*)*)?"...)
+		return
+	}
+	for strings.HasPrefix(c.glob[c.pos:], "**/") {
+		c.regexp = append(c.regexp, "(?:[^/]+(?:/[^/]*)*/)?"...)
+		c.pos += 3
+	}
 }
--- extglob_test.go
+++ extglob_test.go
@@ -18,7 +18,7 @@ var (
 			NonMatches: []string{"asdf/asdf/asdf.jpg", "xxxasdf/asdf.jpgxxx"},
 		},
 		{
-			Glob:       "asdf/**.jpg",
+			Glob:       "asdf/**/*.jpg",
 			Matches:    []string{"asdf/asdf.jpg", "asdf/asdf_asdf.jpg", "asdf/asdf/asdf.jpg", "asdf/asdf/asdf/asdf/asdf.jpg"},
 			NonMatches: []string{"/asdf/asdf.jpg", "asdff/asdf.jpg", "xxxasdf/asdf.jpgxxx"},
 		},
@@ -29,8 +29,253 @@ var (
 		},
 		{
 			Glob:       "**/*.js",
-			Matches:    []string{"asdf/asdf.js", "asdf/asdf/asdfasdf_asdf.js", "/asdf/asdf.js", "/asdf/aasdf-asdf.2.1.4.js"},
-			NonMatches: []string{"/asdf/asdf.jpg", "asdf.js"},
+			Matches:    []string{"asdf/asdf.js", "asdf/asdf/asdfasdf_asdf.js", "asdf/asdf.js", "asdf/aasdf-asdf.2.1.4.js"},
+			NonMatches: []string{"/asdf/asdf.jpg", "asdf/asdf.jpg"},
+		},
+		{
+			Glob:       "ab**",
+			Matches:    []string{"ab", "abcdef", "abef", "abcfef"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "ab***ef",
+			Matches:    []string{"abcdef", "abcfef", "abef"},
+			NonMatches: []string{"ab", "abcfefg"},
+		},
+		{
+			Glob:       "ab**",
+			Matches:    []string{"ab", "abcdef", "abcfef", "abcfefg", "abef"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "ab?bc",
+			Matches:    []string{},
+			NonMatches: []string{"abbc", "abc"},
+		},
+		{
+			Glob:       "[a-d]*.[a-b]",
+			Matches:    []string{"a.a", "a.b", "c.a", "a.a.a"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "*.[a-b]",
+			Matches:    []string{"a.a", "a.b", "a.a.a", "c.a"},
+			NonMatches: []string{"d.a.d", "a.bb", "a.ccc", "c.ccc"},
+		},
+		{
+			Glob:       "*.[a-b]*",
+			Matches:    []string{"a.a", "a.b", "a.a.a", "c.a", "d.a.d", "a.bb"},
+			NonMatches: []string{"a.ccc", "c.ccc"},
+		},
+		{
+			Glob:       "*[a-b].[a-b]*",
+			Matches:    []string{"a.a", "a.b", "a.a.a", "a.bb"},
+			NonMatches: []string{"c.a", "d.a.d", "a.ccc", "c.ccc"},
+		},
+		{
+			Glob:       "[a-y]*[^c]",
+			Matches:    []string{"abd", "abe", "bb", "bcd", "ca", "cb", "dd", "de"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "ab*(e|f)",
+			Matches:    []string{"ab", "abef"},
+			NonMatches: []string{"abcdef", "abcfef", "abcfefg"},
+		},
+		{
+			Glob:       "*(f*(o))",
+			Matches:    []string{"fofo", "ffo", "foooofo", "foooofof", "fooofoofofooo"},
+			NonMatches: []string{"xfoooofof", "foooofofx", "ofooofoofofooo"},
+		},
+		{
+			Glob:       "*(f*(o)x)",
+			Matches:    []string{"foooxfooxfoxfooox", "foooxfooxfxfooox"},
+			NonMatches: []string{"foooxfooxofoxfooox"},
+		},
+		{
+			Glob:       "*(*(of*(o)x)o)",
+			Matches:    []string{"ofxoofxo", "ofxoofxo", "ofoooxoofxo", "ofoooxoofxoofoooxoofxo", "ofoooxoofxoofoooxoofxoo", "ofoooxoofxoofoooxoofxoo"},
+			NonMatches: []string{"ofoooxoofxoofoooxoofxofo"},
+		},
+		{
+			Glob:       "*(@(a))a@(c)",
+			Matches:    []string{"aac", "ac", "aaac"},
+			NonMatches: []string{"c", "baaac"},
+		},
+		{
+			Glob:       "@(ab|a*(b))*(c)d",
+			Matches:    []string{"acd", "abbcd"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "@(b+(c)d|e*(f)g?|?(h)i@(j|k))",
+			Matches:    []string{"effgz", "efgz", "egz"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "*(oxf+(ox))",
+			Matches:    []string{"oxfoxoxfox"},
+			NonMatches: []string{"oxfoxfox"},
+		},
+		{
+			Glob:       "a(*b",
+			Matches:    []string{"a(b", "a((b", "a((b"},
+			NonMatches: []string{"ab"},
+		},
+		{
+			Glob:       "b?(a|b)",
+			Matches:    []string{"ba"},
+			NonMatches: []string{"a"},
+		},
+		{
+			Glob:       "a*?(x)",
+			Matches:    []string{"a", "ab", "ax"},
+			NonMatches: []string{"ba"},
+		},
+		{
+			Glob:       "a?(x)",
+			Matches:    []string{"a", "ax"},
+			NonMatches: []string{"ab", "ba"},
+		},
+		{
+			Glob:       "?",
+			Matches:    []string{"a"},
+			NonMatches: []string{"aa", "aab"},
+		},
+		{
+			Glob:       "??",
+			Matches:    []string{"aa"},
+			NonMatches: []string{"a", "aab"},
+		},
+		{
+			Glob:       "a??b",
+			Matches:    []string{"aaab"},
+			NonMatches: []string{"a", "aa", "aab"},
+		},
+		{
+			Glob:       "ab**(e|f)",
+			Matches:    []string{"ab", "abcdef", "abef", "abcfef"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "ab**(e|f)g",
+			Matches:    []string{},
+			NonMatches: []string{"ab", "abcdef", "abef", "abcfef"},
+		},
+		{
+			Glob:       "ab*+(e|f)",
+			Matches:    []string{"abcdef", "abcfef", "abef"},
+			NonMatches: []string{"ab", "abcfefg"},
+		},
+		{
+			Glob:       "ab?*(e|f)",
+			Matches:    []string{"abef", "abcfef", "abd"},
+			NonMatches: []string{"123abc", "ab", "abcdef", "abcfefg", "acd"},
+		},
+		{
+			Glob:       "ab*d+(e|f)",
+			Matches:    []string{"abcdef"},
+			NonMatches: []string{"123abc", "ab", "abcfefg", "abef", "abcfef", "abd", "acd"},
+		},
+		{
+			Glob:       "(a+|b)+",
+			Matches:    []string{},
+			NonMatches: []string{"ab", "abcdef", "abcfefg", "abef", "abcfef", "abd", "acd"},
+		},
+		{
+			Glob:       "(a+|b)*",
+			Matches:    []string{},
+			NonMatches: []string{"b", "zz", "abcdef", "abcfefg", "abef", "abcfef", "abd", "acd"},
+		},
+		{
+			Glob:       "!*.(a|b)*",
+			Matches:    []string{},
+			NonMatches: []string{"a.a", "a.b", "a.a.a", "c.a", "d.a.d", "a.bb", "a.ccc"},
+		},
+		{
+			Glob:       "(a|d).(a|b)*",
+			Matches:    []string{},
+			NonMatches: []string{"a.a", "a.b", "a.bb"},
+		},
+		{
+			Glob:       "*.+(b|d)",
+			Matches:    []string{"a.b", "d.a.d", "a.bb"},
+			NonMatches: []string{"a.a", "a.a.a", "c.a", "a.", "a.ccc"},
+		},
+		{
+			Glob:       "a+(b|c)d",
+			Matches:    []string{"abd", "acd"},
+			NonMatches: []string{"abc"},
+		},
+		{
+			Glob:       "a[b*(foo|bar)]d",
+			Matches:    []string{"abd"},
+			NonMatches: []string{"abc", "acd"},
+		},
+		{
+			Glob:       "**/*.js",
+			Matches:    []string{"a.js", "a/a.js", "a/a/b.js"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "a/b/**/*.js",
+			Matches:    []string{"a/b/z.js", "a/b/c/z.js"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "**/*.md",
+			Matches:    []string{"foo.md", "foo/bar.md"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "**/*",
+			Matches:    []string{"ab/a/d", "ab/b", "a/b/c/d/a.js", "a/b/c.js", "a.js", "za.js", "ab", "a.b"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "foo/**/",
+			Matches:    []string{"foo/", "foo/bar/baz/qux/"},
+			NonMatches: []string{"foo/bar", "foo/bazbar", "foo/barbar", "foo/bar/baz/qux"},
+		},
+		{
+			Glob:       "a[^[:alnum:]]b",
+			Matches:    []string{"a.b", "a,b", "a:b", "a-b", "a;b", "a b", "a_b"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "a[-.,:; _]b",
+			Matches:    []string{"a.b", "a,b", "a:b", "a-b", "a;b", "a b", "a_b"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "a@([^[:alnum:]])b",
+			Matches:    []string{"a.b", "a,b", "a:b", "a-b", "a;b", "a b", "a_b"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "a@([-.,:; _])b",
+			Matches:    []string{"a.b", "a,b", "a:b", "a-b", "a;b", "a b", "a_b"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "a@([.])b",
+			Matches:    []string{"a.b"},
+			NonMatches: []string{"a,b", "a:b", "a-b", "a;b", "a b", "a_b"},
+		},
+		{
+			Glob:       "a@([^.])b",
+			Matches:    []string{"a,b", "a:b", "a-b", "a;b", "a b", "a_b"},
+			NonMatches: []string{"a.b"},
+		},
+		{
+			Glob:       "a+([^[:alnum:]])b",
+			Matches:    []string{"a.b", "a,b", "a:b", "a-b", "a;b", "a b", "a_b"},
+			NonMatches: []string{},
+		},
+		{
+			Glob:       "a@(.|[^[:alnum:]])b",
+			Matches:    []string{"a.b", "a,b", "a:b", "a-b", "a;b", "a b", "a_b"},
+			NonMatches: []string{},
 		},
 	}
 )
@@ -41,6 +286,7 @@ func Test_CompileExtGlob(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Couldn’t compile glob %s: %s", entry.Glob, err)
 		}
+		t.Logf("Compiled glob %s: %s", entry.Glob, r)
 		for _, match := range entry.Matches {
 			if !r.MatchString(match) {
 				t.Fatalf("%s didn’t match %s", entry.Glob, match)
